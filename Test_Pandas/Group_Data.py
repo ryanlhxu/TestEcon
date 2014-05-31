5c1062811cb9c37ff1df99eade5129605fe00c52
ryanlhxu <ryanlhxu@gmail.com>
2014-06-01 02:21:16 +0800

    add 1989

--- Test_Pandas/Group_Data.py
+++ Test_Pandas/Group_Data.py
@@ -11,7 +11,7 @@ total_year=2007-1998+1
 # exclude one-year data
 length_year = (panel.groupby('id')['year'].size())
 panel = panel[-panel.id.isin(length_year[length_year==1].index)]
-panel.to_csv('panel.csv')
+panel.to_csv('panel.csv',index=False, index_label=False)
 del panel
 
 # read the panel
@@ -19,13 +19,13 @@ panel = read_csv('panel.csv')
 
 # select the balanced panel
 balanced_panel = panel[panel.id.isin(length_year[length_year==total_year].index)]
-balanced_panel.to_csv('balanced_panel.csv')
+balanced_panel.to_csv('balanced_panel.csv',index=False, index_label=False)
 del balanced_panel
 
 
 # acquire the unbalanced panel which needs to deal with
 ub_panel =  panel[-panel.id.isin(length_year[length_year==total_year].index)]
-ub_panel.to_csv('ub_panel.csv')
+ub_panel.to_csv('ub_panel.csv',index=False,index_label=False)
 del panel
 del ub_panel
 
@@ -36,9 +36,9 @@ ub_panel = read_csv('ub_panel.csv')
 ub_gap_year = ub_panel.groupby('id')['year'].apply(max)-ub_panel.groupby('id')['year'].apply(min)+1
 ub_length_year = ub_panel.groupby('id')['year'].size()
 ub_panel1=ub_panel[ub_panel.id.isin(ub_length_year.index[ub_length_year==ub_gap_year])]
-ub_panel1.to_csv('ub_panel1.csv')
+ub_panel1.to_csv('ub_panel1.csv',index=False,index_label=False)
 ub_panel2=ub_panel[-ub_panel.id.isin(ub_length_year.index[ub_length_year==ub_gap_year])]
-ub_panel2.to_csv('ub_panel2.csv')
+ub_panel2.to_csv('ub_panel2.csv',index=False,index_label=False)
 del ub_panel
 del ub_panel1
 del ub_panel2
